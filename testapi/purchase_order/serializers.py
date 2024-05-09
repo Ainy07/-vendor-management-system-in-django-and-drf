59e6f5c1bd09d03ea2dff06f94b663e4c6b93dcf
ainy07 <ainygupta00@gmail.com>
2024-05-09 22:15:58 +0530

    task

---
+++ testapi/purchase_order/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from .models import PurchaseOrder
+
+
+
+class PurchaseOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseOrder
+        fields = '__all__'
+
+    # Validates the quantity field
+    def validate_quantity(self,value):
+        if value <= 0:
+            raise serializers.ValidationError('Quantity Cannot be less than 1')
+        return value
+
+    # Validates the status field
+    # def validate_status(self,value):
+    #     current_status = self.instance.status
+
+    #     if current_status and current_status.lower() != 'pending': #
+    #         raise serializers.ValidationError('Status cannot be changed')
+        
+    #     if value.lower() not in ['completed','cancelled']:
+    #         raise serializers.ValidationError('Please Enter Valid Status')
+        
+    #     return value.lower()
+    
+    # class PurchaseOrderSerializer(serializers.ModelSerializer):
+    #   ...
+
+    def validate_status(self, value):
+        # Check if status is provided
+        if value is None:
+            raise serializers.ValidationError("Status is required.")
+
+        # Check if status is valid
+        if value not in ['pending', 'completed', 'cancelled']:
+            raise serializers.ValidationError("Invalid status provided.")
+
+        return value
+
+
+    # Validates the quality_rating field
+    def validate_quality_rating(self,value):
+        current_status = self.instance.status
+        if current_status.lower() != 'completed':
+            raise serializers.ValidationError('Cannot give Quality rating ')
+        if value not in range(1,11):
+            raise serializers.ValidationError('Quality Rating must between 1-10')
+        
+        return value
+        
+            
+
